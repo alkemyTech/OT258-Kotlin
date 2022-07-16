0f56d5497ef08049e06db4dd9a8d82e9cc6fd9e8
Andres Andreoli <andresandreolii@gmail.com>
2022-07-16 14:30:43 -0300

    Main navigation added

--- app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.melvin.ongandroid.view
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMainNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    // This function show us the funcionality of the main navigation. MainActivity had to inherit BottomNavigationView class to be able to use this function.
+    // This function will be removed after deploying the fragments.
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.nav_Inicio -> {
+                Toast.makeText(this, "Inicio", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.nav_Staff -> {
+                Toast.makeText(this, "Staff", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.nav_News -> {
+                Toast.makeText(this, "Novedades", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.nav_Social -> {
+                Toast.makeText(this, "Contribuir", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.nav_Contact -> {
+                Toast.makeText(this, "Contacto", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return false
     }
 }
