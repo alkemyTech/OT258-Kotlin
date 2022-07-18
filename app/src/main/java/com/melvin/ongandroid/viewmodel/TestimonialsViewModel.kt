a2fcdb07eb6f581bb2a6aed7095c37252174ea41
pedra <pedranicolas>
2022-07-18 19:15:38 -0300

    Use Case Implementation

--- app/src/main/java/com/melvin/ongandroid/viewmodel/TestimonialsViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/TestimonialsViewModel.kt
@@ -3,6 +3,7 @@ package com.melvin.ongandroid.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.melvin.ongandroid.businesslogic.GetTestimonialsUseCase
 import com.melvin.ongandroid.model.testimonials.DataModel
 import com.melvin.ongandroid.model.testimonials.TestimonialRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,17 +11,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TestimonialsViewModel @Inject constructor(private val testimonialRepository: TestimonialRepository) :
+class TestimonialsViewModel @Inject constructor(private val getTestimonialsUseCase: GetTestimonialsUseCase) :
     ViewModel() {
     val testimonialModel = MutableLiveData<List<DataModel>>()
     val isLoading = MutableLiveData<Boolean>()
 
-    //This function refresh the testimonials livedata value with the apis response.
+    //This function refresh the testimonials livedata value with the use case response.
     //The "isLoading" value is waiting the query completion, can be use in a future for the implementation of the loading spinner
     fun onCreate() {
         viewModelScope.launch {
             isLoading.postValue(true)
-            val result = testimonialRepository.getAllTestimonials()
+            val result = getTestimonialsUseCase()
             if (result.isNotEmpty()) {
                 testimonialModel.postValue(result)
                 isLoading.postValue(false)
