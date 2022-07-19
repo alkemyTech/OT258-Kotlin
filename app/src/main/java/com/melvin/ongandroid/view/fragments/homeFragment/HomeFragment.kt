d34183a54fb667d7dca1e3fe359e97a7cd136990
GonzaGuzman <gonzaguzmanz@gmail.com>
2022-07-19 13:20:12 -0300

    add snackbar

--- app/src/main/java/com/melvin/ongandroid/view/fragments/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/homeFragment/HomeFragment.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
+import com.google.android.material.snackbar.Snackbar
+import com.melvin.ongandroid.R
 import com.melvin.ongandroid.databinding.FragmentHomeBinding
 import com.melvin.ongandroid.model.testimonials.DataModel
 import com.melvin.ongandroid.view.adapters.testimonials.TestimonialsAdapter
@@ -22,7 +24,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private val testimonialsViewModel: ViewModel by viewModels()
+    private val viewModel: ViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -40,11 +42,12 @@ class HomeFragment : Fragment() {
         getTestimonials()
         testimonialsArrowClick()
         initWelcomeRecyclerView()
+        if (viewModel.validate()) showSnackBar(resources.getString(R.string.generalError))
     }
     //This function start the testimonials query, an gives the response to the recyclerview
     private fun getTestimonials() {
-        testimonialsViewModel.onCreate()
-        testimonialsViewModel.testimonialModel.observe(viewLifecycleOwner, Observer {
+        viewModel.onCreate()
+        viewModel.testimonialModel.observe(viewLifecycleOwner, Observer {
             initTestimonialRecyclerView(it)
         })
     }
@@ -65,4 +68,11 @@ class HomeFragment : Fragment() {
         snapHelper.attachToRecyclerView(binding.welcomeActivitiesRecyclerView)
     }
 
+//fun to show snackbar and option to refresh view
+    fun showSnackBar(message: String) =
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(resources.getString(R.string.retry)) { viewModel.refreshButton()}
+            .setBackgroundTint(resources.getColor(R.color.red))
+            .setActionTextColor(resources.getColor(R.color.white))
+            .show()
 }
--- app/src/main/java/com/melvin/ongandroid/viewmodel/ViewModel.kt
+++ app/src/main/java/com/melvin/ongandroid/viewmodel/ViewModel.kt
@@ -1,8 +1,13 @@
 package com.melvin.ongandroid.viewmodel
 
+import android.content.res.Resources
+import android.graphics.Color.red
+import androidx.core.content.res.ResourcesCompat.getColor
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.material.snackbar.Snackbar
+import com.melvin.ongandroid.R
 import com.melvin.ongandroid.businesslogic.GetTestimonialsUseCase
 import com.melvin.ongandroid.model.testimonials.DataModel
 import com.melvin.ongandroid.model.testimonials.TestimonialRepository
@@ -13,7 +18,12 @@ import javax.inject.Inject
 @HiltViewModel
 class ViewModel @Inject constructor(private val getTestimonialsUseCase: GetTestimonialsUseCase) :
     ViewModel() {
+    private lateinit var resources: Resources
     val testimonialModel = MutableLiveData<List<DataModel>>()
+    //slideModel and NewModel they are not complete yet use list<String> for example
+    val slideModel = MutableLiveData<List</*SlideDataModel*/String >>()
+    val newsModel = MutableLiveData<List</*newsModel*/String>>()
+
     val isLoading = MutableLiveData<Boolean>()
 
     //This function refresh the testimonials livedata value with the use case response.
@@ -28,4 +38,15 @@ class ViewModel @Inject constructor(private val getTestimonialsUseCase: GetTesti
             }
         }
     }
+
+    //fun to validate the list of requested models from the API
+    fun validate() = (testimonialModel.value.isNullOrEmpty() && slideModel.value.isNullOrEmpty() && newsModel.value.isNullOrEmpty())
+
+    //fun to refresh view if all models empty
+    fun refreshButton(){
+        onCreate()
+        //onCreateSlideModel()
+        //onCreateNewsModel()
+    }
+
 }
