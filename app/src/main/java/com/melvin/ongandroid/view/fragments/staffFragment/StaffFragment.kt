2bc11cacd73251101753273731980607f6740f42
Andres Andreoli <andresandreolii@gmail.com>
2022-07-26 12:35:29 -0300

    some bugs fixed

--- app/src/main/java/com/melvin/ongandroid/view/fragments/staffFragment/StaffFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/staffFragment/StaffFragment.kt
@@ -18,7 +18,6 @@ import com.melvin.ongandroid.view.adapters.staff.StaffAdapter
 import com.melvin.ongandroid.viewmodel.Status
 import com.melvin.ongandroid.viewmodel.ViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class StaffFragment : Fragment() {
@@ -26,12 +25,9 @@ class StaffFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    @Inject
-    private lateinit var firebaseAnalytics: FirebaseAnalytics
-
     private lateinit var binding: FragmentStaffBinding
     private val viewModel: ViewModel by viewModels()
-
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -95,7 +91,7 @@ class StaffFragment : Fragment() {
     private fun onItemSelected(staffDataModel: StaffDataModel) {
         val bundle = Bundle()
         val bundleLog = Bundle()
-
+        firebaseAnalytics = FirebaseAnalytics.getInstance(requireContext())
         val description = deleteHTML(staffDataModel.description.toString())
         bundle.putString("name", staffDataModel.name)
         bundle.putString("roll", description)
