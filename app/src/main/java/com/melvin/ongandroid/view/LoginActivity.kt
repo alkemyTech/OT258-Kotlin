6c3ad8dc5c9bd1d8c9a587970222bcae1d75b176
Julissa JOSEPH <julissajoseph9@gmail.com>
2022-07-28 20:25:17 -0300

    Notificación mediante Firebase a todos los dispositivos

--- app/src/main/java/com/melvin/ongandroid/view/LoginActivity.kt
+++ app/src/main/java/com/melvin/ongandroid/view/LoginActivity.kt
@@ -2,11 +2,19 @@ package com.melvin.ongandroid.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.messaging.FirebaseMessaging
 import com.melvin.ongandroid.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Notificacion()
     }
+
+    private fun Notificacion() {
+        FirebaseMessaging.getInstance().subscribeToTopic("Prueba")
+    }
+
 }
