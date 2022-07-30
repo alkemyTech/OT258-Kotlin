5569888234a0105a0bbc765de9c528927045dec2
GonzaGuzman <gonzaguzmanz@gmail.com>
2022-07-29 21:00:23 -0300

    validate fields contact

--- app/src/main/java/com/melvin/ongandroid/view/fragments/contactFragment/ContactFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/contactFragment/ContactFragment.kt
@@ -5,56 +5,85 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.TransitionBuilder.validate
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.melvin.ongandroid.R
+import com.melvin.ongandroid.databinding.FragmentActivitiesBinding
+import com.melvin.ongandroid.databinding.FragmentContactBinding
+import com.melvin.ongandroid.util.checkMail
+import com.melvin.ongandroid.util.checkMessage
+import com.melvin.ongandroid.util.checkName
+import com.melvin.ongandroid.viewmodel.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class ContactFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentContactBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: ViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        _binding = FragmentContactBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContactFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            validateData()
+            activeButton()
+        }
+    }
+
+    // function to validate that the fields are correctly completed if it does not show the corresponding error message
+    private fun validateData() {
+        with(binding) {
+            etName.doAfterTextChanged {
+                if (!it.toString().checkName()) {
+                    tfName.isErrorEnabled = true
+                    tfName.error =
+                        getString(R.string.error_contact_name_warning)
+                } else
+                    tfName.isErrorEnabled = false
+                viewModel.setContactName(it.toString())
+
             }
+
+            etMail.doAfterTextChanged {
+                if (!it.toString().checkMail()) {
+                    tfMail.isErrorEnabled = true
+                    tfMail.error =
+                        getString(R.string.error_contact_mail_warning)
+                } else
+                    tfMail.isErrorEnabled = false
+                viewModel.setContactMail(it.toString())
+            }
+
+            etMessage.doAfterTextChanged {
+                if (!it.toString().checkMessage()) {
+                    tfMessage.isErrorEnabled = true
+                    tfMessage.error =
+                        getString(R.string.error_contact_message_warning)
+                } else
+                    tfMessage.isErrorEnabled = false
+                viewModel.setContactMessage(it.toString())
+            }
+        }
+    }
+
+    //fun to Activate and deactivate send button
+    private fun activeButton() {
+        viewModel.isButtonEnabled.observe(viewLifecycleOwner) {
+            viewModel.validateDataContact()
+            binding.btnSend.isEnabled = it
+        }
     }
 }
