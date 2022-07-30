0b142b740969ab03c1c62a49a3951de70865183b
Andres Andreoli <andresandreolii@gmail.com>
2022-07-30 09:15:39 -0300

    listeners doOnTextChanged in Email and Login added

--- app/src/main/java/com/melvin/ongandroid/view/fragments/loginFragment/LoginFragment.kt
+++ app/src/main/java/com/melvin/ongandroid/view/fragments/loginFragment/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import com.melvin.ongandroid.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,4 +21,24 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+        initComponent()
+    }
+
+    private fun initComponent(){
+        binding.etEmailLogin.doOnTextChanged { text, start, before, count ->  manageButtonLogin()}
+        binding.etPasswordLogin.doOnTextChanged { text, start, before, count ->  manageButtonLogin()}
+    }
+
+    private fun manageButtonLogin(){
+        /*if (ValidateEmail.isEmail(binding.etEmailLogin.text.toString())){
+            binding.loginBtn.isEnabled = true
+        } else {
+            binding.loginBtn.isEnabled = false
+        }
+    }*/
 }
